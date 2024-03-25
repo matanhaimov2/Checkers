49afd78d4e395f8b21a75f43ef7f5f4f91a44021
unknown <matanhaimov2@gmail.com>
2024-03-25 11:58:08 +0200

    changes 8

--- src/Pages/Board/subComponents/cubeLoc.jsx
+++ src/Pages/Board/subComponents/cubeLoc.jsx
@@ -10,9 +10,10 @@ class Cube extends Component {
     render() {
 
         const { pos } = this.props;
+    
   
         return (
-            <div id={pos} key={pos} pos={pos} onClick={() => this.props.handleMove} className={`checkers-object-wrapper ${(((this.props.xIndex) + (this.props.boardY.length - (this.props.yIndex + 1))) % 2 === 0 ? "dark" : "light")}`}>
+            <div id={pos} key={pos} pos={pos} onClick={() => this.props.handleMove(pos, this.props.boardX, this.props.boardY)} className={`checkers-object-wrapper ${(((this.props.xIndex) + (this.props.boardY.length - (this.props.yIndex + 1))) % 2 === 0 ? "dark" : "light")}`}>
                 {this.props.color!=='empty' && (
                     <Pawn key={pos} id={`${pos}-pawn`} color={this.props.color} />
                 )}
--- src/Pages/GameLogic/gamelogic.jsx
+++ src/Pages/GameLogic/gamelogic.jsx
@@ -15,14 +15,16 @@ import Board from '../Board/board';
 // This file is creating the board and set the pieces
 export default function GameLogic() {
 
-    // Turn machanizem
+    
     const [turn, setTurn] = useState(true); // True means white is playing.
+    const [currentList, setCurrentList] = useState(); // when turn is true => currentList = listOfWhite
+    const [previousPossibleMoves, setPreviousPossibleMoves] = useState(); // previous possible moves
 
     // create a function that first places the pieces. - by placing you need to set in the pos the a1...
     var listOfWhite = ['a6', 'a8', 'b7', 'c6', 'c8', 'd7', 'e6', 'e8', 'f7', 'g6', 'g8', 'h7'];
     var listOfBlack = ['h1', 'h3', 'g2', 'f1', 'f3', 'e2', 'd1', 'd3', 'c2', 'b1', 'b3', 'a2'];
 
-   
+    // Turn machanizem
     useEffect(() => {
         if (turn) {
             startMove('white');
@@ -33,7 +35,7 @@ export default function GameLogic() {
     }, [turn])
 
     const startMove = (currentPlayer) => {
-        let currentList = currentPlayer==='white' ? listOfWhite : listOfBlack;
+        setCurrentList(currentPlayer === 'white' ? listOfWhite : listOfBlack);
 
 
         // Start turn for currentPlayer (white/black)
@@ -43,17 +45,108 @@ export default function GameLogic() {
 
     }
 
-    const getPossibleMoves = () => {
+    const getPossibleMoves = (pos, boardX, boardY) => {
+        console.log(pos)
+        console.log(currentList)
+
+        if (currentList.includes(pos)) {
+            let indexOfX = boardX.indexOf(pos[0]) // pos[0] = b, indexOfX => 7       
+            let indexOfY = boardY.indexOf(parseInt(pos[1])) // pos[1] = 7, indexOfY => 6  
+    
+            let letterAfter = boardX[indexOfX + 1] // c
+            let numberAfter = boardY[indexOfY + 1] // 8
+            let letterBefore = boardX[indexOfX - 1] // a
+            let numberBefore = boardY[indexOfY - 1] // 6
+    
+            let possibleMoves = [];
+            let filteredPossibleMoves = [];
+
+            // if there previousPossivleMoves remove background color - when player changes chosen pawn 
+            if(previousPossibleMoves) {
+                for(let previousPossibleMove of previousPossibleMoves) {
+                    document.getElementById(previousPossibleMove).style.background = null;
+                }
+            }
+    
+            // give player optionsToMove, but filter moving forwards (so player won't move backwards)
+            if (letterAfter) {
+                if (listOfWhite.includes(pos)) { // if pawn is white
+                    possibleMoves.push(numberBefore ? (letterAfter + numberBefore) : null) // 6 => c6
+                }
+                else { // if pawn is black
+                    possibleMoves.push(numberAfter ? (letterAfter + numberAfter) : null) // 8 => c8
+                }
+            }
+            if (letterBefore) {
+                if (listOfWhite.includes(pos)) { // if pawn is white
+                    possibleMoves.push(numberBefore ? (letterBefore + numberBefore) : null) // 6 => a6
+                }
+                else { // if pawn is black
+                    possibleMoves.push(numberAfter ? (letterBefore + numberAfter) : null) // 8 => a8
+                }
+            }
+
+            possibleMoves = possibleMoves.filter(n => n) // filter 'null' out
+
+            // filter own pawns so player won't be able to move on it's own pawns
+            for (let possibleMove of possibleMoves) {
+                var filterOwnPawns = currentList.includes(possibleMove)
+
+                if (!filterOwnPawns) { // if pawn is not on the list => add pawn to filteredPossibleMoves
+                    filteredPossibleMoves.push(possibleMove)
+                }
+            }
+            console.log(filteredPossibleMoves) // filtered possibleMoves 
+
+            // add style for possible moves
+            for (let filteredPossibleMove of filteredPossibleMoves) {
+                let element = document.getElementById(filteredPossibleMove);
+
+                element.style.background = "blue";
+                element.addEventListener("click", () => setPiece(pos, filteredPossibleMove));
+
+
+            }
+
+            setPreviousPossibleMoves(filteredPossibleMoves)
+
+
+        }
+        else {
+            console.log('its not your turn! or empty cube')
+        }
+
+
+
         // Gets the possible moves. filters them out (if there is an other piece there or...)
         // Paints them and adds event listeners to those cubes with the function setPiece
-        console.log('hi')
     }
 
-    const setPiece = () => {
+    const setPiece = (pos, filteredPossibleMove) => {
+        
+        if(listOfWhite.includes(pos)) {
+            for(let i=0; i<listOfWhite.length; i++) {
+                if(listOfWhite[i]===pos) {
+                    listOfWhite.push(filteredPossibleMove) // doesnt save in listofwhite
+                }
+            }
+        }
+        else if(listOfBlack.includes(pos)) {
+            for(let i=0; i<listOfBlack.length; i++) {
+                if(listOfBlack[i]===pos) {
+                    listOfBlack[i] = filteredPossibleMove
+                }
+            }
+        }
+        console.log('nicely done!')
+        console.log(listOfWhite)
+
+
+
         // Call from the event lisenter only!
 
         // Set peice here....
-        console.log('hi')
+        // console.log('hi')
 
 
         // After piece was set - That means Turn has ended (change to next player):
@@ -64,10 +157,10 @@ export default function GameLogic() {
     //     console.log(pos)
     //     let whiteLocations = listOfWhite;
     //     let blackLocations = listOfBlack;
-        
+
     //     // gets previousPossibleMoves from localstorage
     //     let previousPossibleMoveLS = localStorage.getItem("previousPossibleMoves");
-        
+
     //     // if there perviousPossibleMoves => remove blue background color
     //     if(previousPossibleMoveLS) {
     //         for(let previousPossibleMove of JSON.parse(previousPossibleMoveLS)) {
@@ -110,7 +203,7 @@ export default function GameLogic() {
 
     //         possibleMoves = possibleMoves.filter(n => n) // filter 'null' out
     //         console.log(possibleMoves)
-      
+
     //         // filter own pawns so player won't be able to move on it's own pawns
     //         for (let possibleMove of possibleMoves) {
     //             if(whiteLocations.includes(pos)) { // if pawn is white
@@ -152,8 +245,8 @@ export default function GameLogic() {
     //                 if(possibleMove===pos) {
     //                     console.log('player did select this cube', possibleMove)
     //                     console.log(whiteLocations)
-                        
-              
+
+
     //                     for(let i = 0; i<whiteLocations.length; i++) {
     //                         if(whiteLocations[i]===InitialPosLS) {
     //                             this.setState({
@@ -163,7 +256,7 @@ export default function GameLogic() {
     //                     }
 
     //                     console.log(whiteLocations,'good?')
-                        
+
     //                     // Delete the pawn's figure
     //                     let pawnToErase = document.getElementById(`${InitialPosLS}-pawn`);
 
@@ -191,7 +284,7 @@ export default function GameLogic() {
 
     return (
         <div className='checkers-outer-wrapper'>
-            <Board whites={listOfWhite} blacks={listOfBlack} handleMove={getPossibleMoves}/>
+            <Board whites={listOfWhite} blacks={listOfBlack} handleMove={getPossibleMoves} />
         </div>
     )
 }
